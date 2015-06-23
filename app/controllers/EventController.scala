2e43d233ef802c612dac4e1bf154366e7257d5b0
Henrique Martins <hmodinos@gmail.com>
2015-06-23 16:50:59 -0300

    Implemented the Event usecase

---
+++ app/controllers/EventController.scala
@@ -0,0 +1,71 @@
+package controllers
+
+import models.{Event, Events}
+import play.api.data.Form
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+/**
+ * Created by henrique on 23/06/15.
+ */
+class EventController extends Controller {
+
+  implicit val writes = Json.writes[Event]
+  implicit val reads = Json.reads[Event]
+
+
+
+  val eventForm = Form(
+    mapping(
+      "name" -> text,
+      "date" -> date("yyyy-MM-dd"),
+      "clientMax" -> of[Int],
+      "maleTicket" ->  of[Int],
+      "femaleTicket" -> of[Int],
+      "description" -> optional(text),
+      "style" -> optional(text)
+    )(Event.apply)(Event.unapply)
+  )
+
+  def list = Action(parse.empty) { implicit request => Ok(Json.toJson(Events.all)) }
+
+  def create = Action(parse.json) { implicit request =>
+    eventForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(formWithErrors.errorsAsJson),
+      (model: Event) => {
+        Events create(model) match {
+          case true => Ok
+          case _ => BadRequest
+        }
+      }
+    )
+  }
+
+  def find(name: String) = Action(parse.empty) { implicit request =>
+    Events find(name) match {
+      case club => Ok(Json.toJson(club))
+      case _ => NotFound("There is no event with the given id")
+    }
+  }
+
+  def update = Action(parse.json) { implicit request =>
+    eventForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(formWithErrors.errorsAsJson),
+      (model: Event) => {
+        Events update(model) match {
+          case 1 => Ok
+          case _ => BadRequest
+        }
+      }
+    )
+  }
+
+  def delete(name: String) = Action(parse.json) { implicit request =>
+    Events delete(name) match {
+      case 1 => Ok
+      case _ => NotFound("There is no event with the given id")
+    }
+  }
+}
---
+++ app/models/Event.scala
@@ -0,0 +1,55 @@
+package models
+
+import java.util.Date
+
+import scala.slick.driver.PostgresDriver
+import scala.slick.driver.PostgresDriver.simple._
+import play.api.Play.current
+import play.api.data.Forms._
+
+/**
+ * Created by henrique on 23/06/15.
+ */
+case class Event(
+                      name: String,
+                      date : Date,
+                      clientMax: Int,
+                      maleTicket: Int,
+                      femaleTicket: Int,
+                      description : Option[String] = None,
+                      style :  Option[String] = None)
+
+class Events(tag: Tag) extends Table[Event](tag,"event"){
+  def name = column[String]("name", O.PrimaryKey)
+  def date = column[String]("date", O.NotNull)
+  def clientMax = column[String]("clientMax", O.NotNull)
+  def maleTicket = column[String]("maleTicket", O.NotNull)
+  def femaleTicket = column[String]("femaleTicket", O.NotNull)
+  def description = column[String]("description")
+  def style = column[String]("style")
+
+  def * = (name, date, clientMax, maleTicket, femaleTicket, description.?, style.?) <> (Event.tupled, Event.unapply)
+}
+
+object Events {
+  val db = play.api.db.slick.DB
+  val events = TableQuery[Events]
+  def all: List[Event] = db.withSession { implicit session =>
+    events.sortBy(_.name.asc).list
+  }
+  def create(event: Event): Boolean = db.withTransaction{ implicit session =>
+    if (!events.filter(_.name === event.name).exists.run) {
+      events += event
+      true
+    } else {false}
+  }
+  def find(name: String): Event = db.withSession{ implicit session =>
+    events.filter(_.name === name).first
+  }
+  def update(updateEvent: Event): Int = db.withTransaction{ implicit session =>
+    events.filter(_.name === updateEvent.name).update(updateEvent)
+  }
+  def delete(name: String): Int = db.withTransaction{ implicit session =>
+    events.filter(_.name === name).delete
+  }
+}
